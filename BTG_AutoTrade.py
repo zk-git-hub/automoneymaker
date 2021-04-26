1f8ca8f82967a2eb2a1a170ccd4fdf60729f6ce1
zk-git-hub <83182751+zk-git-hub@users.noreply.github.com>
2021-04-26 17:34:05 +0900

    Add files via upload

--- BTG_AutoTrade.py
+++ BTG_AutoTrade.py
@@ -48,30 +48,30 @@ def get_current_price(ticker):
 
 # 로그인
 upbit = pyupbit.Upbit(access, secret)
-print("XRP_autotrade start")
+print("BTG_autotrade start")
 # 시작 메세지 슬랙 전송
-post_message(myToken,"#amm", "XRP_autotrade start")
+post_message(myToken,"#amm", "BTG_autotrade start")
 
 while True:
     try:
         now = datetime.datetime.now()
-        start_time = get_start_time("KRW-XRP")
+        start_time = get_start_time("KRW-BTG")
         end_time = start_time + datetime.timedelta(days=1)
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-XRP", 0.3)
-            ma15 = get_ma15("KRW-XRP")
-            current_price = get_current_price("KRW-XRP")
+            target_price = get_target_price("KRW-BTG", 0.3)
+            ma15 = get_ma15("KRW-BTG")
+            current_price = get_current_price("KRW-BTG")
             if target_price < current_price and ma15 < current_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
-                    buy_result = upbit.buy_market_order("KRW-XRP", krw*0.49975)
-                    post_message(myToken,"#amm", "XRP buy : " +str(buy_result))
+                    buy_result = upbit.buy_market_order("KRW-BTG", krw*0.49975)
+                    post_message(myToken,"#amm", "BTG buy : " +str(buy_result))
         else:
-            xrp = get_balance("XRP")
-            if xrp > 0.00008:
-                sell_result = upbit.sell_market_order("KRW-XRP", xrp*0.9995)
-                post_message(myToken,"#amm", "XRP buy : " +str(sell_result))
+            btg = get_balance("BTG")
+            if btg > 0.00008:
+                sell_result = upbit.sell_market_order("KRW-BTG", btg*0.9995)
+                post_message(myToken,"#amm", "BTG buy : " +str(sell_result))
         time.sleep(1)
     except Exception as e:
         print(e)
