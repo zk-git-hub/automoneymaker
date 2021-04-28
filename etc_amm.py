e2da1de88bc01a73bf0cbdf744c1d5f6f67b5816
zk-git-hub <83182751+zk-git-hub@users.noreply.github.com>
2021-04-29 03:54:35 +0900

    Update etc_amm.py

--- etc_amm.py
+++ etc_amm.py
@@ -59,7 +59,7 @@ while True:
         end_time = start_time + datetime.timedelta(days=1)
 
         if start_time < now < end_time - datetime.timedelta(seconds=1200):
-            target_price = get_target_price("KRW-ETC", 0.1)
+            target_price = get_target_price("KRW-ETC", 0.5)
             btc = get_balance("ETC")
             krw = get_balance("KRW")
             current_price = get_current_price("KRW-ETC")
